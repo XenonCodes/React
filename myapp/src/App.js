40d69f22845133789539488198a81aea2aa5a768
Xenon <SDA152nn@gmail.com>
2022-10-04 05:25:39 +0300

    Task completed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,31 +1,32 @@
 import './App.css';
+import Form from './components/Form';
+import Message from './components/Message';
+import ChatList from './components/ChatList';
 import React, { useState, useEffect } from 'react'
 
 function App() {
-  const [messageList, setMessageList] = useState([]) // 1) Добавить в компонент App поле стейта messageList. В нем хранить массив объектов - сообщений (объект должен содержать, как минимум, поля text и author). Начальное значение - пустой массив).
+  const [messageList, setMessageList] = useState([]) 
   const [messageBody, setMessageBody] = useState({
     text: '',
     author: ''
   })
 
-  //4. Добавить в App useEffect - на каждое отправленное пользователем сообщение должен отвечать робот (должно автоматически отправляться сообщение с фиксированным текстом) - для этого необходимо проверять автора последнего сообщения.
   const textBot = "Lorem ipsum dolor, sit amet consectetur adipisicing elit."
   useEffect(() => {
     if (messageList.length > 0 && messageList.slice(-1)[0].author !== 'Bot') {
-      //5. * Сделать задержку ответа робота - сообщение от него должно отправляться через некоторый промежуток времени после отправки сообщения пользователя (напр., 1.5 сек).
       setTimeout(() => {
         setMessageList(pervstate => [...pervstate, { text: textBot, author: 'Bot' }])
       }, 1500)
     }
   }, [messageList])
 
-  //3. Добавить и стилизовать форму - поле для ввода текста и кнопка для отправки сообщения. При отправке сообщения обновление UI должно происходить за счет обновления стейта App (messageBody, setMessageBody).
   return (
     <div className="App">
-      <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList}></Form>
-      <div className="messageList">
+      <ChatList />
+      <Form data={messageBody} setData={setMessageBody} setMessage={setMessageList} messageList={messageList} />
+      <div className="message-list">
         {
-          messageList.map((el, i) => <Message author={el.author} text={el.text} key={i} />) //2.) Рендерить список сообщений через map.
+          messageList.map((el, i) => <Message author={el.author} text={el.text} key={i} />) // 3. Исправить ошибку в консоли, связанную с отсутствием key у сообщений.
         }
       </div>
     </div>
@@ -33,57 +34,3 @@ function App() {
 }
 
 export default App;
-
-const Form = ({ data, setData, setMessage }) => {
-  const { text, author } = data
-  const submitForm = (el) => {
-    el.preventDefault()
-    if (text.length > 0) {
-      setMessage(pervstate => [...pervstate, { text, author }])
-    }
-    setData(
-      {
-        text: '',
-        author: ''
-      }
-    )
-  }
-
-  return (
-    <div className="message-box">
-      <h2>Message</h2>
-      <form onSubmit={submitForm}>
-        <div className="user-box">
-          <input placeholder='Name' value={author} onChange={(el) =>
-            setData(pervstate => ({ ...pervstate, author: el.target.value }))
-          } />
-          <label>Username</label>
-        </div>
-        <div className="user-box">
-          <input placeholder='Text' value={text} onChange={(el) =>
-            setData(pervstate => ({ ...pervstate, text: el.target.value }))
-          } />
-          <label>Text message</label>
-        </div>
-        <button className='btn' type='submit'>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          Send
-        </button>
-      </form>
-    </div>
-  )
-}
-
-const Message = ({ author, text }) => {
-
-  return (
-    <div className='message'>
-      <h3>{author}</h3>
-      <hr></hr>
-      <p className='message__text'>{text}</p>
-    </div>
-  )
-}
